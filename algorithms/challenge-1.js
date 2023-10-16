c0a01e643fcd49f12711852a0ccc9a7694371a1d
DevelopedbyDavid <fivelanes72@gmail.com>
2023-10-16 13:38:14 -0700

    added comments

--- algorithms/challenge-1.js
+++ algorithms/challenge-1.js
@@ -1,7 +1,7 @@
 function countCharOccurrences(str, char) {
-const regExp = new RegExp(char, 'g');
-const match = str.match(regExp);
-return match ? match.length : 0;
+const regExp = new RegExp(char, 'g');  // Create a regular expression for the character
+const match = str.match(regExp); // Use the regular expression to find all matches
+return match ? match.length : 0; // Return the count of matches or 0 if there are no matches
 }
 console.log(countCharOccurrences('hello', 'l'));
 module.exports = countCharOccurrences;
